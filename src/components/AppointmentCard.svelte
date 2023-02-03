0adac8e98a8ad20a00c0ddb672d7bc331ba183b4
Dan Alin <alin.ioan.dan@accenture.com>
2023-02-03 23:13:16 +0100

    fix: img src in appointment card

--- src/components/AppointmentCard.svelte
+++ src/components/AppointmentCard.svelte
@@ -7,9 +7,9 @@
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class="card w-full   bg-base-300 shadow-xl mx-auto h-64 " on:click={handleClick}>
+<div class="card w-full   bg-base-300 shadow-xl mx-auto h-64 image-full" on:click={handleClick}>
 	{#if event.image}
-		<figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+		<figure><img src={event.image} alt="card" /></figure>
 	{/if}
 
 	<div class="card-body">
