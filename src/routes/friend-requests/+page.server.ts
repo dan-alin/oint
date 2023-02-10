9dc3896bd3217b2c144cf1c43b59b13b27fb4d2f
Francesco <malagisifra@gmail.com>
2023-02-10 12:53:00 +0100

    fix: remove console.log

--- src/routes/friend-requests/+page.server.ts
+++ src/routes/friend-requests/+page.server.ts
@@ -3,7 +3,6 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (event) => {
 	const { cookies } = event;
 	const userToken = cookies.get('session');
-	console.log(userToken);
 	const response = await fetch('https://oint-ms.vercel.app/friends/pending-list', {
 		method: 'GET',
 		headers: {
