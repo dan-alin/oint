6d24ee882cf7a5ce9b24d7de331429fdda7acaa9
Dan Alin <alin.ioan.dan@accenture.com>
2023-02-11 02:41:20 +0100

    chore:add appointment nav in home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,19 @@
-<!-- TODO resolve flicker when drawer is open -->
-<div class="w-full flex justify-center items-center gap-6">
+<!-- TODO page layout-->
+<div class="w-full flex flex-col justify-center items-center gap-6">
 	<a href="/login">
-		<button class="btn btn-primary">login</button>
+		<button class="btn btn-primary w-40">login</button>
 	</a>
 	<a href="/sign-up">
-		<button class="btn btn-primary">sign up</button>
+		<button class="btn btn-primary w-40">sign up</button>
 	</a>
 	<a href="/add-friends">
-		<button class="btn btn-primary">add friends</button>
+		<button class="btn btn-primary w-40">add friends</button>
 	</a>
-	<a
-		href="/friend-requests
-	"
-	>
-		<button class="btn btn-primary">friendd request</button>
+	<a href="/friend-requests">
+		<button class="btn btn-primary w-40">friendd request</button>
+	</a>
+
+	<a href="/appointments">
+		<button class="btn btn-primary w-40">appointments</button>
 	</a>
 </div>
