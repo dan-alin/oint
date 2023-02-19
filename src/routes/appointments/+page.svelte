d8c7ec7b1f20310704abfd9095f2db64117e1067
Alin <danalin06@gmail.com>
2023-02-19 17:36:18 +0100

    feat: add calendar icon for card component

--- src/routes/appointments/+page.svelte
+++ src/routes/appointments/+page.svelte
@@ -88,7 +88,7 @@
 </div>
 
 <div
-	class="flex shadow-md justify-center items-center h-20  w-full fixed bottom-0 left-0 bg-base-200 rounded-t-2xl z-50"
+	class="flex shadow-md justify-center items-center h-24  w-full fixed bottom-0 left-0 bg-base-200 rounded-t-2xl z-50"
 >
 	<label for="create-appointment-modal" class="btn btn-circle">+</label>
 </div>
