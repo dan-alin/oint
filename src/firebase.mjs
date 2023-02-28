92b8299c365b19c1dfa56e7589915b9566cba07a
Dan Alin <alin.ioan.dan@accenture.com>
2023-02-28 21:38:27 +0100

    test: firebase.mjs

--- src/firebase.mjs
+++ src/firebase.mjs
@@ -1,27 +1,27 @@
 import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
-// const {
-// 	VITE_FIREBASE_API_KEY,
-// 	VITE_FIREBASE_AUTH_DOMAIN,
-// 	VITE_FIREBASE_DATABASE_URL,
-// 	VITE_FIREBASE_PROJECT_ID,
-// 	VITE_FIREBASE_STORAGE_BUCKET,
-// 	VITE_FIREBASE_MESSAGING_SENDER_ID,
-// 	VITE_FIREBASE_APP_ID,
-// 	VITE_FIREBASE_MEASUREMENT_ID,
-// 	VITE_FIREBASE_VAPID_KEY
-// } = import.meta.env;
+const {
+	VITE_FIREBASE_API_KEY,
+	VITE_FIREBASE_AUTH_DOMAIN,
+	VITE_FIREBASE_DATABASE_URL,
+	VITE_FIREBASE_PROJECT_ID,
+	VITE_FIREBASE_STORAGE_BUCKET,
+	VITE_FIREBASE_MESSAGING_SENDER_ID,
+	VITE_FIREBASE_APP_ID,
+	VITE_FIREBASE_MEASUREMENT_ID,
+	VITE_FIREBASE_VAPID_KEY
+} = import.meta.env;
 
 const firebaseConfig = {
-	apiKey: process.env.VITE_FIREBASE_API_KEY,
-	authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
-	projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.VITE_FIREBASE_APP_ID,
-	measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
+	apiKey: VITE_FIREBASE_API_KEY,
+	authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+	databaseURL: VITE_FIREBASE_DATABASE_URL,
+	projectId: VITE_FIREBASE_PROJECT_ID,
+	storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: VITE_FIREBASE_APP_ID,
+	measurementId: VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -29,7 +29,7 @@ const messaging = getMessaging(firebaseApp);
 
 export const getTokenFirebase = (/** @type {(arg0: string) => void} */ setTokenFound) => {
 	getToken(messaging, {
-		vapidKey: process.env.VITE_FIREBASE_VAPID_KEY
+		vapidKey: VITE_FIREBASE_VAPID_KEY
 	})
 		.then((currentToken) => {
 			if (currentToken) {
