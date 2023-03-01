4a703bbf9367b6db905e33dd928d514a5942ef26
Francesco <malagisifra@gmail.com>
2023-03-02 00:16:36 +0100

    dsds

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,7 @@ import { VITE_FIREBASE_API_KEY } from '$env/static/private';
 import type { PageServerLoad } from './appointments/$types';
 
 export const load: PageServerLoad = async (event) => {
-	const a = VITE_FIREBASE_API_KEY.split('');
+	const a = VITE_FIREBASE_API_KEY[0];
 	return {
 		VITE_FIREBASE_API_KEY: a
 	};
