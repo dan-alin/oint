f5cbe0c46c98af3e21ef77824c1a11c762b7bbc7
Francesco <malagisifra@gmail.com>
2023-03-02 00:17:17 +0100

    fdfd

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,6 +4,6 @@ import type { PageServerLoad } from './appointments/$types';
 export const load: PageServerLoad = async (event) => {
 	const a = VITE_FIREBASE_API_KEY[0];
 	return {
-		VITE_FIREBASE_API_KEY: a
+		VITE_FIREBASE_API_KEY: !!a
 	};
 };
