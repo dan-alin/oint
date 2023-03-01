ca04cc89710ba9f6f297d4371ba68dc751a7e388
Francesco <malagisifra@gmail.com>
2023-03-01 23:51:19 +0100

    ewew

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
-import type { PageServerLoad } from './$types';
 import { VITE_FIREBASE_API_KEY } from '$env/static/private';
+import type { PageServerLoad } from './appointments/$types';
 
 export const load: PageServerLoad = async (event) => {
 	console.log('ENV', VITE_FIREBASE_API_KEY);
