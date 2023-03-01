f8936e185e724a268e40ddacf2fddd80ef7534b2
Francesco <malagisifra@gmail.com>
2023-03-02 00:20:28 +0100

    fedf

--- src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { VITE_FIREBASE_API_KEY } from '$env/static/private';
-import type { PageServerLoad } from './appointments/$types';
-
-export const load: PageServerLoad = async (event) => {
-	const a = VITE_FIREBASE_API_KEY[0];
-	return {
-		VITE_FIREBASE_API_KEY: !!a
-	};
-};
