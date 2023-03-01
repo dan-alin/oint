2ad2eb0e31d63143f3b5869cb86272f6a0d05369
Francesco <malagisifra@gmail.com>
2023-03-01 23:54:57 +0100

    dsds

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,8 +2,6 @@ import { VITE_FIREBASE_API_KEY } from '$env/static/private';
 import type { PageServerLoad } from './appointments/$types';
 
 export const load: PageServerLoad = async (event) => {
-	console.log('ENV', VITE_FIREBASE_API_KEY);
-
 	return {
 		env: VITE_FIREBASE_API_KEY
 	};
