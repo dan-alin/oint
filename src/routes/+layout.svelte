c2754b9a10794afdabff7bd969208f810eae493a
Dan Alin <alin.ioan.dan@accenture.com>
2023-03-02 15:14:52 +0100

    feat:push notificationb

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,23 +10,23 @@
 	import Alert from '../components/Alert.svelte';
 	import { toggleAlert, type AlertState } from '../stores/alert';
 	import { onMount } from 'svelte';
-	const {
-		VITE_FIREBASE_API_KEY,
-		VITE_FIREBASE_AUTH_DOMAIN,
-		VITE_FIREBASE_DATABASE_URL,
-		VITE_FIREBASE_PROJECT_ID,
-		VITE_FIREBASE_STORAGE_BUCKET,
-		VITE_FIREBASE_MESSAGING_SENDER_ID,
-		VITE_FIREBASE_APP_ID,
-		VITE_FIREBASE_MEASUREMENT_ID,
-		VITE_FIREBASE_VAPID_KEY
-	} = import.meta.env;
+	// const {
+	// 	VITE_FIREBASE_API_KEY,
+	// 	VITE_FIREBASE_AUTH_DOMAIN,
+	// 	VITE_FIREBASE_DATABASE_URL,
+	// 	VITE_FIREBASE_PROJECT_ID,
+	// 	VITE_FIREBASE_STORAGE_BUCKET,
+	// 	VITE_FIREBASE_MESSAGING_SENDER_ID,
+	// 	VITE_FIREBASE_APP_ID,
+	// 	VITE_FIREBASE_MEASUREMENT_ID,
+	// 	VITE_FIREBASE_VAPID_KEY
+	// } = import.meta.env;
 
 	let ReloadPrompt: any;
 	let showSpinner = false;
 	let showAlert: AlertState;
 	let tokeFirebase = '';
-	export let data: { env: any };
+	// export let data: { env: any };
 	console.log(process.env, 'vercel');
 
 	toggleSpinner.subscribe((value) => (showSpinner = value));
@@ -39,22 +39,22 @@
 	};
 
 	onMount(async () => {
-		console.log(
-			'firebaseToken',
-			'import.meta\u200b.env.MODE',
-			VITE_FIREBASE_API_KEY,
-			VITE_FIREBASE_APP_ID,
-			VITE_FIREBASE_AUTH_DOMAIN,
-			VITE_FIREBASE_DATABASE_URL,
-			VITE_FIREBASE_MEASUREMENT_ID,
-			VITE_FIREBASE_MESSAGING_SENDER_ID,
-			VITE_FIREBASE_PROJECT_ID,
-			VITE_FIREBASE_STORAGE_BUCKET,
-			VITE_FIREBASE_VAPID_KEY
-		);
+		// console.log(
+		// 	'firebaseToken',
+		// 	'import.meta\u200b.env.MODE',
+		// 	VITE_FIREBASE_API_KEY,
+		// 	VITE_FIREBASE_APP_ID,
+		// 	VITE_FIREBASE_AUTH_DOMAIN,
+		// 	VITE_FIREBASE_DATABASE_URL,
+		// 	VITE_FIREBASE_MEASUREMENT_ID,
+		// 	VITE_FIREBASE_MESSAGING_SENDER_ID,
+		// 	VITE_FIREBASE_PROJECT_ID,
+		// 	VITE_FIREBASE_STORAGE_BUCKET,
+		// 	VITE_FIREBASE_VAPID_KEY
+		// );
 		const { getTokenFirebase } = await import('../firebase');
 
-		getTokenFirebase(setFirebaseToken);
+		 getTokenFirebase(setFirebaseToken);
 	});
 </script>
 
