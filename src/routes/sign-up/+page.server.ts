1bec7283d873e26d402656822c76b5558dbdefbe
Dan Alin <alin.ioan.dan@accenture.com>
2023-03-06 21:41:32 +0100

    fix: favicons

--- src/routes/sign-up/+page.server.ts
+++ src/routes/sign-up/+page.server.ts
@@ -2,6 +2,7 @@ import type { PhonePrefix } from '../../models';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
+	const { VITE_API_HOST } = import.meta.env;
 	const response = await fetch(`${VITE_API_HOST}phone-prefix`, {
 		method: 'GET',
 		headers: {
