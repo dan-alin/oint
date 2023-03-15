a184268acf33f45b9dd1c8a71d62727a0570f3a2
Dan Alin <alin.ioan.dan@accenture.com>
2023-03-16 00:27:42 +0100

    fix: bg login image

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -43,55 +43,61 @@
 </svelte:head>
 
 <!-- <h1 class="text-2xl font-bold capitalize flex justify-center">{$t('login.title')}</h1> -->
-<img class="mb-6 w-full" src="/backgrounds/login.svg" alt="a door and keys" />
 
-<form on:submit|preventDefault={onSubmit} class="flex flex-col gap-6 px-8">
-	<div class="flex flex-col gap-1">
-		<h2 class="text-2xl">Rieccoti!</h2>
-		<p class="text-md font-light ">
-			Torniamo a pianificare questa settimana, pronto ad organizzare qualcosa di nuovo?
-		</p>
-	</div>
+<div class="background-login bg-container">
+	<form on:submit|preventDefault={onSubmit} class="flex flex-col gap-4 px-8">
+		<div class="flex flex-col gap-1">
+			<h2 class="text-2xl">Rieccoti!</h2>
+			<p class="text-md font-light ">
+				Torniamo a pianificare questa settimana, pronto ad organizzare qualcosa di nuovo?
+			</p>
+		</div>
 
-	<!-- TODO extract classes in an @apply -->
-	<div class="grid grid-cols-1  gap-6 md:grid-cols-3 ">
-		<InputText type="email" id="email" name="email" placeholder="Email" required={true} value="" />
-	</div>
+		<!-- TODO extract classes in an @apply -->
+		<div class="grid grid-cols-1  gap-6 md:grid-cols-3 ">
+			<InputText
+				type="email"
+				id="email"
+				name="email"
+				placeholder="Email"
+				required={true}
+				value=""
+			/>
+		</div>
 
-	<div class="grid grid-cols-1 gap-6 md:grid-cols-3 ">
-		<InputText
-			type="password"
-			id="password"
-			name="password"
-			placeholder="Password"
-			required={true}
-			value=""
-		/>
-	</div>
+		<div class="grid grid-cols-1 gap-6 md:grid-cols-3 ">
+			<InputText
+				type="password"
+				id="password"
+				name="password"
+				placeholder="Password"
+				required={true}
+				value=""
+			/>
+		</div>
 
-	<div class="flex items-center justify-between">
-		<Checkbox
-			label="Rimani connesso"
-			onChange={() => (keepAccess = !keepAccess)}
-			checked={keepAccess}
-		/>
-		<a class=" text-xs text-secondary underline " href="/">Password dimenticata?</a>
-	</div>
+		<div class="flex items-center justify-between">
+			<Checkbox
+				label="Rimani connesso"
+				onChange={() => (keepAccess = !keepAccess)}
+				checked={keepAccess}
+			/>
+			<a class=" text-xs text-secondary underline " href="/">Password dimenticata?</a>
+		</div>
 
-	<div class="grid grid-cols-1  gap-6 md:grid-cols-3">
-		<button class="btn-primary btn-sm btn capitalize" type="submit">Accedi</button>
+		<div class="grid grid-cols-1  gap-6 md:grid-cols-3">
+			<button class="btn-primary btn-sm btn capitalize" type="submit">Accedi</button>
+		</div>
+	</form>
+	<div class="divider px-6" />
+	<div class="flex justify-center text-xs">
+		<span>Non hai ancora un account?</span>
+		<a class="text-secondary underline" href="/sign-up">Registrati</a>
 	</div>
-</form>
-<div class="divider px-6" />
-<div class="flex justify-center text-xs">
-	<span>Non hai ancora un account?</span>
-	<a class="text-secondary underline" href="/sign-up">Registrati</a>
 </div>
 
 <style>
-	.bg-image {
-		background-image: url('/backgrounds/login.svg');
-		background-repeat: no-repeat;
-		background-size: contain;
+	.background-login {
+		background-image: url('/backgrounds/Login_1.svg');
 	}
 </style>
