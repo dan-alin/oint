c73e893313321be14b9eb5cdb1d0da78d8014781
Francesco <malagisifra@gmail.com>
2023-03-31 10:31:51 +0200

    feat: add banner in request sent

--- src/components/friends/AddFriendCard.svelte
+++ src/components/friends/AddFriendCard.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { Icons } from '../../enums';
 	import type { Friend } from '../../models/friend';
+	import BannerRequestSent from '../appointments/BannerRequestSent.svelte';
 	import Icon from '../Icon.svelte';
 
 	export let friend: Friend;
+
 	export let action: (friendId: number) => void = () => null;
 </script>
 
@@ -27,10 +29,19 @@
 			</div>
 		</div>
 	</div>
+	
+	{#if friend.isPending}
+		<BannerRequestSent label="Richiesta inviata"/>
+	{/if}
+	{#if friend.isFriend}
+		<BannerRequestSent label="Amici"/>
+	{/if}
+	{#if !friend.isFriend && !friend.isPending}
+		<button on:click={() => action(friend.user.id)} class=" btn-primary btn-sm btn rounded-md">
+			Richiedi amicizia
+		</button>
+	{/if}
 
-	<button on:click={() => action(friend.user.id)} class=" btn-primary btn-sm btn rounded-md">
-		Richiedi amicizia
-	</button>
 
 	<div class="divider my-0" />
 </div>
