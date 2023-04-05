6849175e159633dc7f6ccd5a8cce7f66266c2ce3
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-04-05 13:10:08 +0200

    feat(ci): use envs for output prining

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,3 @@
 RESULT=$(npm run ci:lint 2>&1 || echo "job_status:failed")
 
-            if echo "$RESULT" | grep -q "job_status:failed"; then
-              echo 'status=x'
-              exit 1
-            else
-              echo 'status=white_check_mark'
-            fi
+echo "$RESULT"
