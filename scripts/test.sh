8453166c6b35fca0e0efeffdf21bd5b226739df4
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-04-05 12:07:20 +0200

    feat(ci): update workflow order

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,8 @@
-RESULT=$(npm run ci:format 2>&1 || echo "pipeline_status=failed")
+RESULT=$(npm run ci:lint 2>&1 || echo "job_status:failed")
 
+            if echo "$RESULT" | grep -q "job_status:failed"; then
+              echo 'status=x'
+              exit 1
+            else
+              echo 'status=white_check_mark'
+            fi
