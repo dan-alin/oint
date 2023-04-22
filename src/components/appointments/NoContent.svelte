5e8548c7c28400cb90c965875e405fb10dcd8de9
valerio.pescatori <valerio.pescatori@accenture.com>
2023-04-22 10:35:06 +0200

    fix(notifications): adds no notifications message

--- src/components/appointments/NoContent.svelte
+++ src/components/appointments/NoContent.svelte
@@ -2,14 +2,18 @@
 	import type { Icons } from '../../enums';
 	import Icon from '../Icon.svelte';
 
-	export let icon: Icons;
-	export let size: string;
+	export let icon: Icons | undefined = undefined;
+	export let size = '24';
 	export let heading: string;
 	export let subHeading: string;
 </script>
 
 <div class="mt-28 flex flex-col items-center text-center text-disabled">
-	<Icon {icon} {size} color="var(--color-disabled)" />
+	{#if icon}
+		<Icon {icon} {size} color="var(--color-disabled)" />
+	{:else}
+		<slot />
+	{/if}
 	<h4 class="mt-5 text-sm font-bold">{heading}</h4>
 	<p class="mt-2 text-xs">{subHeading}</p>
 </div>
--- src/routes/notifications/+page.svelte
+++ src/routes/notifications/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import NoContent from '../../components/appointments/NoContent.svelte';
+	import Icon from '../../components/Icon.svelte';
 	import NotificationCard from '../../components/notifications/NotificationCard.svelte';
 	import PageHead from '../../components/PageHead.svelte';
+	import { Icons } from '../../enums';
 	import type { Notification } from '../../models/notification';
 
 	export let data: { myNotifications: Notification[] };
@@ -16,6 +19,20 @@
 	<div>
 		{#each myNotifications as notification}
 			<NotificationCard {notification} action={filterNotifications} />
+		{:else}
+			<NoContent
+				heading="Non hai nessuna nuova notifica"
+				subHeading="Smuoviamo le acque? Non vorrai mica rimanere a casa tutta la settimana?!?"
+			>
+				<div class="rotate-[20deg] relative">
+					<Icon icon={Icons.NOTIFICATION_OUTLINE} size="45" />
+					<span
+						class="bg-light-gray absolute -rotate-[20deg] top-0 right-0 inline-block w-5 h-5 leading-5 rounded-[50%] font-bold text-white text-xs"
+					>
+						0
+					</span>
+				</div>
+			</NoContent>
 		{/each}
 	</div>
 </div>
