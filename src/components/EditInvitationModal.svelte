6dbd9bfb2e056ec35b05a8165d21bed51618d884
valerio.pescatori <valerio.pescatori@accenture.com>
2023-04-29 13:41:33 +0200

    fix(EditInvitationModal): removes postpone invitation option

--- src/components/EditInvitationModal.svelte
+++ src/components/EditInvitationModal.svelte
@@ -7,38 +7,31 @@
 
 	let inputEl: HTMLInputElement;
 	const options = [
-		{ id: 'radio-accept', label: 'Partecipa', value: 0 },
-		{ id: 'radio-refuse', label: 'Rifiuta', value: 1 },
-		{ id: 'radio-postpone', label: 'Posticipa risposta', value: 2 }
+		{ id: 'radio-accept', label: 'Partecipa', value: true },
+		{ id: 'radio-refuse', label: 'Rifiuta', value: false }
 	];
-	$: selected = accepted ? 0 : 1;
-	let initial = selected;
+	$: selected = accepted;
+	$: initial = accepted;
 
 	const editInvitation = async () => {
+		console.log('se', selected, 'initial', initial);
 		if (selected === initial) {
 			inputEl.checked = false;
 			return;
 		}
-		switch (selected) {
-			case 0:
-				await invitationService.acceptAppointment(
-					appointmentId,
-					sessionStorage.getItem('jwt_token') || '',
-					true
-				);
-				break;
-			case 1:
-				await invitationService.declineAppointment(
-					appointmentId,
-					sessionStorage.getItem('jwt_token') || '',
-					true
-				);
-				break;
-			case 2:
-				// TODO: wait for postpone-invitation api
-				break;
+		if (selected) {
+			await invitationService.acceptAppointment(
+				appointmentId,
+				sessionStorage.getItem('jwt_token') || '',
+				true
+			);
+		} else {
+			await invitationService.declineAppointment(
+				appointmentId,
+				sessionStorage.getItem('jwt_token') || '',
+				true
+			);
 		}
-
 		inputEl.checked = false;
 	};
 </script>
