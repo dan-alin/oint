65bfaed17f42cc64b2d7e4bb42acc2bcfc04d20b
Francesco <malagisifra@gmail.com>
2023-05-07 17:43:59 +0200

    feat: add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -86,6 +86,20 @@
 
 <svelte:head>
 	{@html webManifest}
+	<!-- Google Tag Manager -->
+	<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-WT6PRB7');
+	</script>
+	<!-- End Google Tag Manager -->
 </svelte:head>
 
 {#if showSpinner}
